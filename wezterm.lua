8e2c7e21540b23ce7f480e9dbcdd30c819b2a28c
Kyle Ryan Garcia <36806756+AlsoPeters@users.noreply.github.com>
2025-02-22 11:02:39 +0900

    fixed font. Used a more generic user location

--- wezterm.lua
+++ wezterm.lua
@@ -1,17 +1,24 @@
-
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 --[ Color Scheme ]--
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 
 --[ Font ]--
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
 config.font_size = 16
+
+-- Font rules
+config.font_rules = {
+	{
+		italic = true,
+		font = wezterm.font("FiraCode Nerd Font"),
+	},
+}
 
 --[ Tab Bar ]--
 -- config.hide_tab_bar_if_only_one_tab = true
@@ -20,50 +27,54 @@ config.use_fancy_tab_bar = false
 
 --[ Default Program ]--
 config.default_prog = {
-    'wsl.exe',
-    '--cd',
-    '/home/alsopeters',
-    "zsh",
-    '-l'
+	"wsl.exe",
+	"--cd",
+	"~",
+	"zsh",
+	"-l",
 }
 
 --[ Lauch Menu ]--
 
 config.launch_menu = {
-    {
-        label = 'Bash => Windows',
-        args = { 'bash.exe' }
-    },
-    {
-        label = 'PowerShell',
-        args = { "powershell.exe" }
-    },
-    {
-        label = 'CMD',
-        args = { 'cmd.exe' }
-    },
+	{
+		label = "Bash => Windows",
+		args = { "bash.exe" },
+	},
+	{
+		label = "PowerShell",
+		args = { "powershell.exe" },
+	},
+	{
+		label = "CMD",
+		args = { "cmd.exe" },
+	},
 }
 
 --[ Keybindings ]--
 config.keys = {
-    -- Open a new tab with Ctrl+T
-    { key = "t",          mods = "CTRL",       action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-    -- Open Launcher Menu
-    { key = 'l',          mods = 'ALT',        action = wezterm.action.ShowLauncher },
-    -- Split pane vertically with Ctrl+|
-    { key = "|",          mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+	-- Open a new tab with Ctrl+T
+	{ key = "t", mods = "CTRL", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	-- Open Launcher Menu
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+	-- Split pane vertically with Ctrl+|
+	{
+		key = "|",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
 
-    -- Split pane horizontally with Ctrl+-
-    { key = "+",          mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+	-- Split pane horizontally with Ctrl+-
+	{ key = "+", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-    -- Switch between panes with Alt+ArrowKeys
-    { key = "LeftArrow",  mods = "ALT",        action = wezterm.action.ActivatePaneDirection("Left") },
-    { key = "RightArrow", mods = "ALT",        action = wezterm.action.ActivatePaneDirection("Right") },
-    { key = "UpArrow",    mods = "ALT",        action = wezterm.action.ActivatePaneDirection("Up") },
-    { key = "DownArrow",  mods = "ALT",        action = wezterm.action.ActivatePaneDirection("Down") },
+	-- Switch between panes with Alt+ArrowKeys
+	{ key = "LeftArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "RightArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "UpArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "DownArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
 
-    -- Close a tab with Ctrl+W
-    { key = "w",          mods = "CTRL",       action = wezterm.action.CloseCurrentTab { confirm = false } },
+	-- Close a tab with Ctrl+W
+	{ key = "w", mods = "CTRL", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
 }
 
 return config
